05c3a380dec6d72594a7e1c832e6fdb0146625f4
yok2086 <gyurimkimyok@gmail.com>
2025-02-01 01:08:12 +0800

    Add, List

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -7,21 +7,33 @@ public class David {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-
+        String[] values = new String[100];
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm David\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        while(!input.equals("bye")) {
-
-            System.out.println("____________________________________________________________\n"
-                    + input + "!!\n" + "____________________________________________________________");
+        int i = 0;
+        while (!input.equals("bye")) {
+            if(input.equals("list")) {
+                for (int j = 0; j < i ; j++) {
+                    System.out.println("____________________________________________________________\n" + (j + 1) + "." + values[j]);
+                }
+            }
+            else {
+                System.out.println("____________________________________________________________\n"
+                        + "added: " + input + "\n" + "____________________________________________________________");
+                values[i] = input;  // store user input in array
+                i++;
+            }
             input = scanner.nextLine();
-        }
 
-        System.out.println(" Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+
+        }
+            System.out.println(" Bye. Hope to see you again soon!\n" +
+                    "____________________________________________________________");
+        }
     }
-}
+
+
